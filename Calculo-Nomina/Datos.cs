c1ac24f4fdf2b5f97e5ec3ca6a4a90ea79d8b17f
jochimin88 <201905041@uapa.edu.do>
2020-06-03 14:06:47 -0700

    cleaned

--- Calculo-Nomina/Datos.cs
+++ Calculo-Nomina/Datos.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculo_Nomina_Luis_Contreras_2019_04211
+﻿namespace Calculo_Nomina_Luis_Contreras_2019_04211
 {
     public class Datos
     {
-
     }
 }
